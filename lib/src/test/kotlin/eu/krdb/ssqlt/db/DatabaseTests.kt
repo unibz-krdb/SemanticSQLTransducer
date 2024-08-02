eb01150f3655e3d1d8bfa3326f82b3e82b6b83ca
Aidan Bailey <dev@aidanbailey.me>
2024-08-02 17:45:22 +0200

    Add more checks to test

--- lib/src/test/kotlin/eu/krdb/ssqlt/db/DatabaseTests.kt
+++ lib/src/test/kotlin/eu/krdb/ssqlt/db/DatabaseTests.kt
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Assertions.*
 class DatabaseTests {
     @Test fun testConstructor() {
         var db = Database("postgresql", "localhost", "5432", "ssqlt_test")
+        assert(!db.isClosed())
         db.close()
+        assert(db.isClosed())
     }
 }
