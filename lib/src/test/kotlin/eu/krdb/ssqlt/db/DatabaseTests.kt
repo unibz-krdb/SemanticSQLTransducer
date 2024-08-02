77d2c2381b337946b3c9cedd86a5c8a55535fd28
Aidan Bailey <dev@aidanbailey.me>
2024-08-02 10:23:33 +0200

    Change db test to use database ssqlt_test

--- lib/src/test/kotlin/eu/krdb/ssqlt/db/DatabaseTests.kt
+++ lib/src/test/kotlin/eu/krdb/ssqlt/db/DatabaseTests.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions.*
 
 class DatabaseTests {
     @Test fun testConstructor() {
-        var db = Database("postgresql", "localhost", "5432", "postgres")
+        var db = Database("postgresql", "localhost", "5432", "ssqlt_test")
         db.close()
     }
 }
