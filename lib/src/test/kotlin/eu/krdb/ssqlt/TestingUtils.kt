f473366ac44f56eee1229168f15f7e798f95be30
Aidan Bailey <dev@aidanbailey.me>
2024-08-10 23:50:57 +0200

    Fix getResourceFileString

--- lib/src/test/kotlin/eu/krdb/ssqlt/TestingUtils.kt
+++ lib/src/test/kotlin/eu/krdb/ssqlt/TestingUtils.kt
@@ -3,13 +3,7 @@ package eu.krdb.ssqlt
 class TestingUtils {
     companion object {
         fun getResourceFileString(filename: String): String {
-            return object {}
-                .javaClass
-                .getResourceAsStream(filename)
-                ?.bufferedReader()
-                ?.readLines()
-                ?.joinToString("")
-                ?: throw Exception("File not found: $filename")
+            return object {}.javaClass.classLoader.getResource(filename).readText()
         }
     }
 }
