c8e09cb7310edd7f47f97a5837680fff25f0caf1
Aidan Bailey <dev@aidanbailey.me>
2024-08-11 19:59:44 +0200

    Fix resource file string not being trimmed

--- lib/src/test/kotlin/eu/krdb/ssqlt/TestingUtils.kt
+++ lib/src/test/kotlin/eu/krdb/ssqlt/TestingUtils.kt
@@ -3,7 +3,7 @@ package eu.krdb.ssqlt
 class TestingUtils {
     companion object {
         fun getResourceFileString(filename: String): String {
-            return object {}.javaClass.classLoader.getResource(filename).readText()
+            return object {}.javaClass.classLoader.getResource(filename).readText().trim()
         }
     }
 }
