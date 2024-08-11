be84cc70bdb63b36546a53c16781152b924af6ee
Aidan Bailey <dev@aidanbailey.me>
2024-08-11 20:00:08 +0200

    Add implementation for fromString

--- lib/src/main/kotlin/eu/krdb/ssqlt/db/Table.kt
+++ lib/src/main/kotlin/eu/krdb/ssqlt/db/Table.kt
@@ -1,6 +1,7 @@
 package eu.krdb.ssqlt.db
 
 import net.sf.jsqlparser.parser.CCJSqlParserUtil
+import net.sf.jsqlparser.statement.create.table.CreateTable
 import java.nio.file.Path
 import java.nio.file.Files
 import java.nio.charset.StandardCharsets
@@ -24,20 +25,34 @@ class Table(
         }
 
         fun fromString(sql: String): Table {
-            val res = CCJSqlParserUtil.parse(sql)
-            throw NotImplementedError()
+            val parsed = CCJSqlParserUtil.parse(sql)
+            parsed?.let {
+                if (it is CreateTable) {
+                    val schema = it.table.schemaName
+                    val name = it.table.name
+                    val attributes = it.columnDefinitions.map {
+                        Attribute(it.columnName, it.colDataType.dataType, it.columnSpecs.isNullOrEmpty())
+                    }
+                    val primaryKey = it.indexes.find { it.type == "PRIMARY KEY" }?.columns?.map { it.columnName } ?: emptyList()
+                    return Table(schema, name, attributes, primaryKey)
+                }
+                else {
+                    throw Exception("Not a Create Table statement")
+                }
+            }
+            throw Exception("Invalid SQL")
         }
     }
 
     fun toCreateTableSql(): String {
         val builder = StringBuilder()
-        builder.append("CREATE TABLE $schema.$name (\n")
-        builder.append(attributes.joinToString(",\n") { it.toSql() })
+        builder.append("CREATE TABLE $schema.$name(")
+        builder.append(attributes.joinToString(",") { it.toSql() })
         if (primaryKey.isNotEmpty()) {
-            builder.append(",\nPRIMARY KEY (${primaryKey.joinToString(", ")})")
+            builder.append(",PRIMARY KEY (${primaryKey.joinToString(",")})")
         }
-        builder.append("\n)")
-        return builder.toString().trimIndent()
+        builder.append(")")
+        return builder.toString()
     }
 
 }
