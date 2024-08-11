c14bca0f693d68942c43bade898c628913ae6ced
Aidan Bailey <dev@aidanbailey.me>
2024-08-11 19:28:13 +0200

    Add equals function

--- lib/src/main/kotlin/eu/krdb/ssqlt/db/Attribute.kt
+++ lib/src/main/kotlin/eu/krdb/ssqlt/db/Attribute.kt
@@ -8,4 +8,17 @@ class Attribute(
     fun toSql(): String {
         return "$name $type${if (nullable) "" else " NOT NULL"}"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+
+        other as Attribute
+
+        if (name != other.name) return false
+        if (type != other.type) return false
+        if (nullable != other.nullable) return false
+
+        return true
+    }
 }
