b81987f6453070ecf8d0f052cfdd998c990e0992
Aidan Bailey <dev@aidanbailey.me>
2024-08-11 18:28:02 +0200

    Add toSql test

--- lib/src/test/kotlin/eu/krdb/ssqlt/db/AttributeTest.kt
+++ lib/src/test/kotlin/eu/krdb/ssqlt/db/AttributeTest.kt
@@ -12,4 +12,12 @@ class AttributeTest {
         assertEquals("VARCHAR(100)", attribute.type)
         assertEquals(false, attribute.nullable)
     }
+
+    @Test
+    fun testToSql() {
+        var attribute = Attribute("ssn", "VARCHAR(100)", false)
+        assertEquals("ssn VARCHAR(100) NOT NULL", attribute.toSql())
+        attribute = Attribute("ssn", "VARCHAR(100)", true)
+        assertEquals("ssn VARCHAR(100)", attribute.toSql())
+    }
 }
