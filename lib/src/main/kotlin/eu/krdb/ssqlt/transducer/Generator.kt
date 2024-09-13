5ebec20a176b6b4ce9b24ce31fe4d5b85f523605
Aidan Bailey <dev@aidanbailey.me>
2024-09-13 22:48:21 +0200

    Add generateInsertDeleteTables

--- lib/src/main/kotlin/eu/krdb/ssqlt/transducer/Generator.kt
+++ lib/src/main/kotlin/eu/krdb/ssqlt/transducer/Generator.kt
@@ -1,7 +1,35 @@
 package eu.krdb.ssqlt.transducer
 
 import eu.krdb.ssqlt.db.Schema
+import eu.krdb.ssqlt.db.Table
 
 class Generator(val context: Context) {
-    fun generateInsertDeleteTables(schema: Schema) {}
+
+    companion object {
+
+        private fun generateInsertDeleteTables(schema: Schema): List<Table> {
+            val tables = mutableListOf<Table>()
+            for (table in schema.tables) {
+                tables.add(
+                    Table(
+                        table.schemaName,
+                        table.tableName + "_INSERT",
+                        table.attributes,
+                        table.primaryKey))
+                tables.add(
+                    Table(
+                        table.schemaName,
+                        table.tableName + "_DELETE",
+                        table.attributes,
+                        table.primaryKey))
+            }
+            return tables
+        }
+    }
+
+    fun generateInsertDeleteTables(): List<Table> {
+        val tables = listOf<Table>()
+        val schemas = listOf(context.source, context.target)
+        return tables
+    }
 }
