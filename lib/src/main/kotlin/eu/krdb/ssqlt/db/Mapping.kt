a1718781b4b243d32cd478f99d3106b1e5d633f6
Aidan Bailey <dev@aidanbailey.me>
2024-09-13 22:26:32 +0200

    Doc FIXME

--- lib/src/main/kotlin/eu/krdb/ssqlt/db/Mapping.kt
+++ lib/src/main/kotlin/eu/krdb/ssqlt/db/Mapping.kt
@@ -1,3 +1,5 @@
 package eu.krdb.ssqlt.db
 
+// FIXME: This is clearly not enough information to generate the mapping
+// However, I don't think mappings are actually important now
 class Mapping(val source: List<Attribute>, val target: List<Attribute>) {}
