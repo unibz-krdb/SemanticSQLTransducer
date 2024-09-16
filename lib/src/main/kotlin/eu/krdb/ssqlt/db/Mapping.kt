98179199f8dcda8f67980aa9136bab71eb4f357a
Aidan Bailey <dev@aidanbailey.me>
2024-09-16 20:51:16 +0200

    Add toSql function

--- lib/src/main/kotlin/eu/krdb/ssqlt/db/Mapping.kt
+++ lib/src/main/kotlin/eu/krdb/ssqlt/db/Mapping.kt
@@ -6,5 +6,40 @@ class Mapping(
     val schema: String,
     val sourceTable: String,
     val targetTable: String,
-    val attributes: List<Attribute>
-) {}
+    val attributes: List<Attribute>,
+    val nullAttributes: Dictionary<Attribute, Boolean>
+) {
+    fun toSql(
+        includeSchema: Boolean = true,
+        customSourceTable: String? = null
+    ): String {
+
+        var strSourceTable = customSourceTable ?: sourceTable
+        if (includeSchema) {
+            strSourceTable = "$schema.$strSourceTable"
+        }
+
+        // SELECT
+        var sqlString = "SELECT ${attributes.map { a -> strSourceTable + "." + a.name }.joinToString( ", ")}"
+
+        // FROM
+        sqlString += " FROM ${strSourceTable}"
+
+        // WHERE
+        if (nullAttributes.isEmpty()) {
+            sqlString += " WHERE"
+            var andString = " "
+            for (key in nullAttributes.keys()) {
+                sqlString += andString
+                if (!nullAttributes[key]) {
+                    sqlString += "${strSourceTable}.${key.name} IS NOT NULL"
+                } else {
+                    sqlString += "${strSourceTable}.${key.name} IS NULL"
+                }
+                andString = " AND "
+            }
+        };
+
+        return sqlString
+    }
+}
