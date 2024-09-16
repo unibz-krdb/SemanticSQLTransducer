c6aaf5d94df56db598986697c1065a7efa69aefe
Aidan Bailey <dev@aidanbailey.me>
2024-09-16 19:37:28 +0200

    Change Mapping

--- lib/src/main/kotlin/eu/krdb/ssqlt/db/Mapping.kt
+++ lib/src/main/kotlin/eu/krdb/ssqlt/db/Mapping.kt
@@ -1,5 +1,10 @@
 package eu.krdb.ssqlt.db
 
-// FIXME: This is clearly not enough information to generate the mapping
-// However, I don't think mappings are actually important now
-class Mapping(val source: List<Attribute>, val target: List<Attribute>) {}
+import java.util.Dictionary
+
+class Mapping(
+    val schema: String,
+    val sourceTable: String,
+    val targetTable: String,
+    val attributes: List<Attribute>
+) {}
